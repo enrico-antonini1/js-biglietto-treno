c4dbb10eb252c0e1186a66d0acc41d476643f918
enrico-antonini1 <enry1997@me.com>
2025-09-16 13:29:02 +0200

    feat: ragionamento bonus 1

---
+++ bonus/bonus1.js
@@ -0,0 +1,21 @@
+/*
+RACCOLTA DATI
+chiedere all'utente primo numero
+chiedere all'utente secondo numero
+chiedere se vuole somme o differenza
+preparare variabile messaggio
+
+ESECUZIONE LOGICA
+calcolare la somma se scegli somma
+calcolare la differenza se sceglie differenza
+
+SE somma
+    numero 1 + numero 2
+SE DIFFERENZA
+    numero 1 - numero 2
+
+OUTPUT
+stampo messaggio
+*/
+
+
