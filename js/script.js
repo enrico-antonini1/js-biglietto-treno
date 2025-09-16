8b74021a41d7e7339249622c24c047f01f439f7f
enrico-antonini1 <enry1997@me.com>
2025-09-16 12:09:36 +0200

    feat: inserito ragionamento

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* 
+RACCOLTA DATI
+
+chiedere all'utente età
+chiedere all'utente km
+preparare variabile messaggio
+
+ESECUZIONE LOGICA
+calcolare prezzo biglietto 
+p interno = 0,21 * km
+p minorenni = 0,21 * km * (1-0,20)
+p over 65 = 0,21 * km * (1-0,40)
+
+OUTPUT
+stampo messaggio
+*/
