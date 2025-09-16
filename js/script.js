6266e48701344e526a21bba290a02a2fa9d3fb36
enrico-antonini1 <enry1997@me.com>
2025-09-16 13:22:45 +0200

    feat: applicato controllo numero

--- js/script.js
+++ js/script.js
@@ -27,19 +27,24 @@ const kmStr = prompt("Quanti km devi percorrere?");
 const km = parseInt(kmStr);
 const age = parseInt(ageStr)
 
+if (isNaN(ageStr) || isNaN(kmStr)) {
+    console.log("Devi inserire un numero, ricarica la pagina!")
+} else {
+
 const prezzoIntero = 0.21 * km;
 const prezzoU18 = prezzoIntero * (1 - 0.2);
 const prezzoO65 = prezzoIntero * (1 - 0.4);
 
 
 if (age < 18) {
-    message = `Il prezzo del biglietto è ${prezzoU18}`;
+    message = `Il prezzo del biglietto è ${prezzoU18}`;    
 } else if (age >= 65) {
-    message = `Il prezzo del biglietto è ${prezzoO65}`;
-}
-else {
+    message = `Il prezzo del biglietto è ${prezzoO65}`;    
+} else {
     message = `Il prezzo del biglietto è ${prezzoIntero}`;
- }
+  }
 
- console.log(message);
 
+console.log(message);
+
+}
