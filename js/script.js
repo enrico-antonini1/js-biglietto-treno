0c0ec1af6a70ed9363f3a927671e3e525e926bef
enrico-antonini1 <enry1997@me.com>
2025-09-16 13:03:00 +0200

    fix: sistemazione errori

--- js/script.js
+++ js/script.js
@@ -6,36 +6,40 @@ chiedere all'utente km
 preparare variabile messaggio
 
 ESECUZIONE LOGICA
-calcolare prezzo biglietto 
-p interno = 0,21 * km
-p minorenni = 0,21 * km * (1-0,20)
-p over 65 = 0,21 * km * (1-0,40)
+calcolare prezzo biglietto in base all'età
+SE età < 18
+    p minorenni = 0,21 * km * (1-0,20)
+
+SE età > 65
+    p over 65 = 0,21 * km * (1-0,40)
+
+ALTRIMENTI
+    p interno = 0,21 * km
 
 OUTPUT
 stampo messaggio
 */
 
 let message = "";
-const age = prompt("Quanti anni hai?");
+const ageStr = prompt("Quanti anni hai?");
 const kmStr = prompt("Quanti km devi percorrere?");
 
 const km = parseInt(kmStr);
+const age = parseInt(ageStr)
 
 const prezzoIntero = 0.21 * km;
-const prezzo18 = prezzoIntero * (1-0.2);
-const prezzoO65 = prezzoIntero * (1-0.4);
-
-// console.log(prezzoIntero)
-// console.log(prezzo18)
-console.log(prezzoO65)
+const prezzoU18 = prezzoIntero * (1 - 0.2);
+const prezzoO65 = prezzoIntero * (1 - 0.4);
 
 
- if (age <= 18) {
-     message = `Il prezzo del biglietto è ${prezzo18}`;
- } else if (age >= 65) {
-     message = `Il prezzo del biglietto è ${prezzoO65}`;
+if (age < 18) {
+    message = `Il prezzo del biglietto è ${prezzoU18}`;
+} else if (age >= 65) {
+    message = `Il prezzo del biglietto è ${prezzoO65}`;
+}
+else {
+    message = `Il prezzo del biglietto è ${prezzoIntero}`;
  }
- else {
-     message = `Il prezzo del biglietto è ${prezzoInterno}`;
- }
+
+ console.log(message);
 
