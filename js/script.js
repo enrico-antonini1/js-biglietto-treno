3aa11b3d088633c2fa2c18fd84c19859426367de
enrico-antonini1 <enry1997@me.com>
2025-09-16 12:23:39 +0200

    feat: esecuzione logica + messaggio finale

--- js/script.js
+++ js/script.js
@@ -15,6 +15,20 @@ OUTPUT
 stampo messaggio
 */
 
-let message= "";
+let message = "";
 const age = prompt("Quanti anni hai?");
 const km = prompt("Quanti km devi percorrere?");
+
+const prezzoIntero = "0,21" * km;
+const prezzo18 = prezzoIntero * ("1-0,20")
+const prezzoO65 = prezzoIntero * ("1-0,40")
+
+if (age <= 18) {
+    message = `Il prezzo del biglietto è ${prezzo18}`;
+} else if (age >= 65) {
+    message = `Il prezzo del biglietto è ${prezzoO65}`;
+}
+else {
+    message = `Il prezzo del biglietto è ${prezzoInterno}`;
+}
+
